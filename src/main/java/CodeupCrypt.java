11729d955edf7575c2242c88edef6db6df7cf18b
James Wienecke <james.a.wienecke@gmail.com>
2022-01-21 10:43:19 -0600

    Began building out CodeupCrypt to pass the tests.

---
+++ src/main/java/CodeupCrypt.java
@@ -0,0 +1,20 @@
+public class CodeupCrypt {
+    public static double version;
+
+    /* HASHING */
+    /*
+    Chars	Replace
+    a or A	4
+    e or E	3
+    i or I	1
+    o or O	0
+    u or U	9
+     */
+    public static String hashPassword(String str) {
+        return "";
+    }
+
+    public static boolean checkPassword(String plain, String crypt) {
+        return false;
+    }
+}
