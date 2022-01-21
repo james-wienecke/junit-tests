37b133e4ed310f66b1ddd09b650cca93747ff4c1
James Wienecke <james.a.wienecke@gmail.com>
2022-01-21 10:43:46 -0600

    hashPassword test passing.

--- src/main/java/CodeupCrypt.java
+++ src/main/java/CodeupCrypt.java
@@ -11,7 +11,17 @@ public class CodeupCrypt {
     u or U	9
      */
     public static String hashPassword(String str) {
-        return "";
+        return str
+                .replace('a', '4')
+                .replace('A', '4')
+                .replace('e', '3')
+                .replace('E', '3')
+                .replace('i', '1')
+                .replace('I', '1')
+                .replace('o', '0')
+                .replace('O', '0')
+                .replace('u', '9')
+                .replace('U', '9');
     }
 
     public static boolean checkPassword(String plain, String crypt) {
