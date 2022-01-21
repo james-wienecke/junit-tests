017cd44ce2be384193aafa9bf0fdbec1c7344833
James Wienecke <james.a.wienecke@gmail.com>
2022-01-21 11:08:02 -0600

    Began building the Student tests.

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StudentTest {
+    @Test
+    public void studentConstructor() {
+        long id = 1234L;
+        String name = "test";
+        Student student = new Student()
+    }
+}
